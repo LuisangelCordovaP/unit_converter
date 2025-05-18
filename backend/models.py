414f58346b8f585ffbfcf02babbb042112e3d5e9
Luis <tata27_2002@hotmail.com>
2025-05-17 20:47:55 -0600

    feat: add category enum in models for convertionRequest

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,13 @@
 from pydantic import BaseModel
+from enum import Enum
+
+class CategoryEnum(str, Enum):
+    length = 'length'
+    weigth = 'weight'
+    temperature = 'temperature'
 
 class ConvertionRequest(BaseModel):
-    category: str
+    category: CategoryEnum
     from_unit : str
     to_unit : str
     value : float
